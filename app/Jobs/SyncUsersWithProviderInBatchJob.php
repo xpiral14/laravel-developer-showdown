0b2aa13fee5fb578a3c35fac319d58a5fc2df5f0
xpiral14 <sa.reis1999@gmail.com>
2024-10-16 19:44:41 -0300

    feat: added job to sync user with provider

---
+++ app/Jobs/SyncUsersWithProviderInBatchJob.php
@@ -0,0 +1,62 @@
+<?php
+
+namespace App\Jobs;
+
+use App\Http\DataProvider;
+use App\Models\UnsynchronizedUser;
+use App\Models\User;
+use GuzzleHttp\Exception\GuzzleException;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\Collection;
+use Illuminate\Foundation\Queue\Queueable;
+use Illuminate\Queue\Middleware\RateLimitedWithRedis;
+use Illuminate\Support\Facades\Log;
+
+class SyncUsersWithProviderInBatchJob implements ShouldQueue
+{
+    use Queueable;
+
+    public function __construct()
+    {
+    }
+
+    public function handle(): void
+    {
+        $provider = new DataProvider();
+        User::query()
+            ->with('unsynchronizedUser')
+            ->join('unsynchronized_users', 'users.id', '=', 'unsynchronized_users.user_id')
+            ->whereRaw("data->'modified_fields' is not null")
+            ->select('users.*')
+            ->chunkById(
+                $provider::PROVIDER_MAX_CHUNK_SIZE,
+                function (Collection $users) use ($provider) {
+                    $this->synchronizeUsers($users, $provider);
+                }
+            );
+
+    }
+
+    /**
+     * @throws GuzzleException
+     */
+    function synchronizeUsers(Collection $users, DataProvider $provider): void
+    {
+        $provider->sendUnsynchronizedUsersInBatch($users);
+        $this->makeUsersSynchronized($users);
+    }
+
+
+    private function makeUsersSynchronized(Collection $users): void
+    {
+        UnsynchronizedUser::whereIn('id', $users->pluck('id'))->update(['data' => []]);
+    }
+
+
+    public function middleware(): array
+    {
+        return [(new RateLimitedWithRedis('unsynchronized-batch-users'))->dontRelease()];
+    }
+}
+
